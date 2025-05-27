698621d417ee13e4c2df93e285554fe363cd08b8
Cott-AI <gr302568@gradia.fi>
2025-05-27 09:20:17 +0300

    feat(svelte): svelte-kauppa

---
+++ svelte-kauppa/_app/immutable/chunks/CmPPztZw.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_mdp8v2)==null?void 0:s.base)??"/ohjelmointi-portfolio/svelte-kauppa";var e;const a=((e=globalThis.__sveltekit_mdp8v2)==null?void 0:e.assets)??t;export{a,t as b};
